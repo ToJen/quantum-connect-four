ab1f52c9c330af468fecf1db5481a7f840b0f69e
Tomisin Jenrola <tomisinjenrola@gmail.com>
2019-12-12 22:05:01 -0500

    added boilerplate

---
+++ webpack.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+    entry: [
+        './src/index.jsx'
+    ],
+    module: {
+        rules: [
+            {
+                test: /\.(js|jsx)$/,
+                exclude: /node_modules/,
+                use: ['babel-loader']
+            }
+        ]
+    },
+    output: {
+        path: __dirname + '/static',
+        filename: 'bundle.js'
+    }
+};
